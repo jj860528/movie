1e84d0b0ee5f2cbe4c8e0afe7bc559bda289cd56
twtrubiks <jj860528@gmail.com>
2020-06-20 04:25:47 +0800

    create project

--- src/firebase.js
+++
@@ -1,7 +0,0 @@
-import firebase from "firebase";
-import "firebase/firestore";
-import firebaseConfig from "./firebaseConfig";
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-const db = firebaseApp.firestore();
-const database = firebase.database();
-export default { db, database };
--- src/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyBdWxNGdr76zypUCW0dtdegxnZuGYdxcVw",
-  authDomain: "theater-3da46.firebaseapp.com",
-  databaseURL: "https://theater-3da46.firebaseio.com",
-  projectId: "theater-3da46",
-  storageBucket: "theater-3da46.appspot.com",
-  messagingSenderId: "55726216999",
-  appId: "1:55726216999:web:4072e370a91102ab"
-};
